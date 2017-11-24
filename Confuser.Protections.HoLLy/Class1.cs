2ef4024a82b64bd0f4d2a11d3f8e59b2799355c8
HoLLy <dario.db@hotmail.com>
2017-11-25 00:05:00 +0100

    Add project files.

---
+++ Confuser.Protections.HoLLy/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confuser.Protections.HoLLy
+{
+    public class Class1
+    {
+    }
+}
