b69929326ff837d57101f92d55adce8ea083c3c6
HoLLy <dario.db@hotmail.com>
2017-11-25 21:31:53 +0100

    Add anti watermark protection

---
+++ Confuser.Protections.HoLLy/AntiWatermark/AntiWatermarkPhase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Confuser.Core;
+
+namespace Confuser.Protections.HoLLy.AntiWatermark
+{
+	public class AntiWatermarkPhase : ProtectionPhase
+	{
+		public override ProtectionTargets Targets => ProtectionTargets.Modules;
+		public override string Name => "ConfusedBy attribute removal";
+
+		public AntiWatermarkPhase(ConfuserComponent parent) : base(parent) { }
+		protected override void Execute(ConfuserContext context, ProtectionParameters parameters)
+		{
+			//look for watermark and remove it
+		    foreach (var md in context.Modules) {
+		        var attr = md.CustomAttributes.Find("ConfusedByAttribute");
+                if (attr != null)
+                    md.CustomAttributes.Remove(attr);
+		    }
+		}
+	}
+}
---
+++ Confuser.Protections.HoLLy/AntiWatermark/AntiWatermarkProtection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Confuser.Core;
+
+namespace Confuser.Protections.HoLLy.AntiWatermark
+{
+	public class AntiWatermarkProtection : Protection
+	{
+		public override string Name => "Anti Watermark";
+		public override string Description => "Removes the ConfusedBy watermark to prevent ConfuserEx detection.";
+		public override string Id => "anti watermark";
+		public override string FullId => "HoLLy.AntiWatermark";
+		public override ProtectionPreset Preset => ProtectionPreset.Minimum;
+
+		protected override void Initialize(ConfuserContext context) { }
+
+		protected override void PopulatePipeline(ProtectionPipeline pipeline)
+		{
+			//watermark is added in the inspection stage, this executes right after
+			pipeline.InsertPostStage(PipelineStage.Inspection, new AntiWatermarkPhase(this));
+		}
+	}
+}
