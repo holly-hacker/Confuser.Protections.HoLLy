ce0715b5b8317848e7cbe809cc419a53b5679b1f
HoLLy <dario.db@hotmail.com>
2017-11-26 19:34:11 +0100

    Partially implement Memory Protection

---
+++ Confuser.Protections.HoLLy/AntiMemoryEditing/Types/ObfuscatedValue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Confuser.Protections.HoLLy.AntiMemoryEditing.Types
+{
+    internal class ObfuscatedValue<T>
+    {
+        private static readonly Random Rand = new Random();
+        private readonly int _salt;
+        private T _val;
+
+        public T Value
+        {
+            get => Obfuscate(_val, true);
+            set => _val = Obfuscate(value, false);
+        }
+
+        public ObfuscatedValue(T val)
+        {
+            _salt = Rand.Next(int.MinValue, int.MaxValue);
+            Value = val;
+        }
+        
+        //allow normal read/write
+        public static implicit operator T(ObfuscatedValue<T> value) => value.Value;
+        public static implicit operator ObfuscatedValue<T>(T value) => new ObfuscatedValue<T>(value);
+
+        public override string ToString() => Value.ToString();
+
+        private T Obfuscate(T val, bool reverse = false) => Obfuscate(val, _salt, reverse);
+
+        private static T Obfuscate(T currentvalue, int salt, bool reverse)
+        {
+            Type type = currentvalue.GetType();
+
+            switch (currentvalue)
+            {
+                case string s:
+                    return (T)(object)new string(s.Select(a => (char)(a ^ salt)).ToArray());
+                case int i:
+                    return (T)(object)(i ^ salt);
+                case double d:
+                    return (T)(object)(d * (reverse ? 1.0 / salt : salt));
+                case float f:
+                    return (T)(object)(f * (reverse ? 1f / salt : salt));
+                default:
+                    if (type.BaseType == typeof(Enum))
+                        return (T)(object)((int)Convert.ChangeType(currentvalue, typeof(int)) ^ salt);
+                    else
+                        throw new NotSupportedException();
+            }
+        }
+    }
+}
