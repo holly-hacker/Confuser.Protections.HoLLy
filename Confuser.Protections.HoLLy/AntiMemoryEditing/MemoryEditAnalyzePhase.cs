a32fbdf76b1d5479c263a7a3300bea752b5dfad0
HoLLy <dario.db@hotmail.com>
2017-12-14 21:48:20 +0100

    Don't memory obfuscate non-supported types

--- Confuser.Protections.HoLLy/AntiMemoryEditing/MemoryEditAnalyzePhase.cs
+++ Confuser.Protections.HoLLy/AntiMemoryEditing/MemoryEditAnalyzePhase.cs
@@ -17,12 +17,40 @@ namespace Confuser.Protections.HoLLy.AntiMemoryEditing
 
             foreach (IDnlibDef def in parameters.Targets.WithProgress(context.Logger)) {
                 if (def is FieldDef d) {
-                    service.AddToList(d);
-                    context.Logger.DebugFormat("Added {0} to list", d.Name);
+                    if (CorrectType(d)) {
+                        service.AddToList(d);
+                        context.Logger.DebugFormat("Added {0} to list", d.Name);
+                    }
+                    else {
+                        context.Logger.WarnFormat("{0} was marked for memory protection, but type '{1}' is not supported!", d, d.FieldType);
+                    }
                 }
                 else {
-                    context.Logger.WarnFormat("{0} (a {1}) was marked for memory protection, but this is not (yet) supported!", def, def.GetType());
+                    context.Logger.WarnFormat("{0} (of type {1}) was marked for memory protection, but this is not a field!", def, def.GetType());
                 }
+            }
+        }
+
+        private static bool CorrectType(FieldDef f)
+        {
+            switch (f.FieldType.FullName) {
+                case "System.Byte":
+                case "System.SByte":
+                case "System.Int16":
+                case "System.UInt16":
+                case "System.Int32":
+                case "System.UInt32":
+                case "System.Int64":
+                case "System.UInt64":
+
+                case "System.Single":
+                case "System.Double":
+                case "System.Decimal":
+
+                case "System.String":
+                    return true;
+                default:
+                    return false;
             }
         }
     }
